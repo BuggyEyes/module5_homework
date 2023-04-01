2d05a337b2fd09a52248469d066d9e8a64a599ed
Buggyeyes <ovel2405@yandex.ru>
2023-04-02 00:22:57 +0300

    Create module5.js
    done

---
+++ module5.js
@@ -0,0 +1,15 @@
+let map = new Map();
+map.set("Yolo", "string");
+map.set(2, "number");
+map.set(true, "boolean");
+
+
+for (let key of map.keys()) {
+    console.log(key);
+}
+
+for (let elem of map) {
+    console.log(elem);
+}
+
+
